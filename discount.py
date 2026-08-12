1e03f9ff568e3f71a2174b771bca0a003baf7284
ALDOT Course Team <course-team@local>
2026-08-11 20:01:32 -0600

    introduce lint failure

--- discount.py
+++ discount.py
@@ -13,3 +13,5 @@ def calculate_discounted_price(price, customer_type, coupon_code=None, is_holida
     if is_holiday:
         price = price * 0.95
     return round(price, 2)
+
+SUPPORTED_COUPON_CODES = {"SAVE10": "10% off for all customers", "SAVE20": "20% off exclusively for premium customers"}
